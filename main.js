04656afb6601a2f3a0df70ddb12081aab08d848f
Khanh Chung <khanh.d.chung@gmail.com>
2022-12-08 13:09:50 -0800

    Add functions to each numeric key and AC key

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const keys = document.querySelectorAll('.key');
+const inputDisplay = document.querySelector('.input');
+
+keys.forEach(key => {
+  // Write the number clicked on the input display
+  if (key.dataset.key >= '0' && key.dataset.key <= '9') {
+    key.addEventListener('click', e => {
+      inputDisplay.textContent += key.dataset.key
+    });
+  }
+
+  if (key.dataset.key === 'ac') {
+    key.addEventListener('click', e => {
+      inputDisplay.textContent = '';
+    })
+  }
+});
+
 function operate(first, second, operator) {
   switch (operator) {
     case '+':
