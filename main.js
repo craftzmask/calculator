38352c81c711cc661aa6d82647d7ed4dd7000ddd
Khanh Chung <khanh.d.chung@gmail.com>
2022-12-08 23:23:26 -0800

    Prevent multiple zeros that starts with 0 and AC will reset input to 0

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const keys = document.querySelectorAll('.key');
 const inputDisplay = document.querySelector('.input');
+const operators = ['+', '-', '*', '/'];
 
 keys.forEach(key => {
   // Write the number clicked on the input display
   if (key.dataset.key >= '0' && key.dataset.key <= '9') {
     key.addEventListener('click', e => {
       inputDisplay.textContent += key.dataset.key
+      if (inputDisplay.textContent[0] === '0') {
+        inputDisplay.textContent = inputDisplay.textContent.slice(1);
+      }
     });
   }
 
   if (key.dataset.key === 'ac') {
     key.addEventListener('click', e => {
-      inputDisplay.textContent = '';
+      inputDisplay.textContent = '0';
     })
+  }
+
+  if (operators.includes(key.dataset.key)) {
+    console.log(key.dataset.key)
   }
 });
 
